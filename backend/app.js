183667b9f0787ba5c7f27e32a1cdcc8299c58c8c
Sami <sami.bencheikh@metropolia.fi>
2024-12-09 12:50:52 +0200

    first commit

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const jobRouter = require("./routes/jobRouter");
+const userRouter = require("./routes/userRouter");
+const {
+  unknownEndpoint,
+  errorHandler,
+} = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+connectDB();
+
+// app.use(cors(corsOptions));
+app.use(express.json());
+app.use(express.static('view'))
+
+// Use the jobRouter for all "/jobs" routes
+app.use("/api/jobs", jobRouter);
+// Use the userRouter for all "/jobs" routes
+app.use("/api/users", userRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;
